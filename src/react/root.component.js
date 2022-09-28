13594ebc44a2a74fad0b9946fc146380597d2c75
mateocles <mat.bermudez@udla.edu.co>
2022-09-28 17:17:44 -0500

    add: commit initial

---
+++ src/react/root.component.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const App = () => <h1>Hello from React 11</h1>
+
+export default App
---
+++ src/vue/main.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Hello from Vue mm</h1>
+  </div>
+</template>
